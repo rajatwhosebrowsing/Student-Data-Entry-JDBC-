db8c1deaf2d8b6a3dda7e6dd2d3834524f3c913a
Rajat Dwivedi <dwiv9451@gmail.com>
2025-04-25 22:30:53 +0530

    Student Class File

---
+++ Student.java
@@ -0,0 +1,24 @@
+// Represents a Student entity
+public class Student {
+    private int prn;
+    private String name;
+    private java.sql.Date dob;
+    private int[] marks;  // stores three subject marks
+
+    // Constructor
+    public Student(int prn, String name, java.sql.Date dob, int[] marks) {
+        this.prn   = prn;
+        this.name  = name;
+        this.dob   = dob;
+        this.marks = marks;
+    }
+
+        // Getters
+        public int getPrn()           { return prn; }
+        public String getName()       { return name; }
+        public java.sql.Date getDob() { return dob; }
+        public int[] getMarks()       { return marks; }
+    }
+    
+
+
