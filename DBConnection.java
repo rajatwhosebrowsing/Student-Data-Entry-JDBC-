5b229c79601330c9032f96f0a9bb6f4251fbb239
Rajat Dwivedi <dwiv9451@gmail.com>
2025-04-25 22:30:04 +0530

    DataBase Connection File

---
+++ DBConnection.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// Manages JDBC connection to MySQL
+public class DBConnection {
+    private static final String URL      = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String USER     = "root";     
+    private static final String PASSWORD = "2-Liccolony";         
+
+    // Returns a live Connection object
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
