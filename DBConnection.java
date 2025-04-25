eead51eab66964e00413cf41bbfba6fc7b3bdebe
Rajat Dwivedi <dwiv9451@gmail.com>
2025-04-25 22:39:02 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
     private static final String URL      = "jdbc:mysql://localhost:3306/studentdb";
     private static final String USER     = "root";     
     private static final String PASSWORD = "2-Liccolony";         
-
+ 
     // Returns a live Connection object
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
